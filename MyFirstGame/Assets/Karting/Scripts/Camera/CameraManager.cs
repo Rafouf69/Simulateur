42550a4b1b57ad5d43e182f0d9d0164fb220773c
Rafouf69 <raphael.vignon@insa-lyon.fr>
2021-01-04 10:29:05 +0100

    first and third person view

---
+++ MyFirstGame/Assets/Karting/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject[] ListeView;
+    private int taille;
+    private int numView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taille = ListeView.Length;
+        ListeView[0].SetActive(true);
+        for(int i=0; i<taille; i++)
+        {
+            ListeView[i].SetActive(false);
+        }
+        numView = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Debug.Log("change camera");
+            ListeView[numView].SetActive(false);
+            numView += 1;
+            if (numView == taille)
+            {
+                numView = 0;
+            }
+            ListeView[numView].SetActive(true);
+        }
+    }
+}
---
+++ MyFirstGame/Assets/Karting/Scripts/Camera/FirstPerson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+    public GameObject kart;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = kart.transform.forward * 5.0f;
+
+        transform.position = kart.transform.position + offset;
+    }
+}
