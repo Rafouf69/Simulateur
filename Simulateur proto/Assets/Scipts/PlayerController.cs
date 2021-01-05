4b28e2c76d09edda0c7d5037b83d0808e7f8a87c
GrannySmith21 <bastien.barbe65@gmail.com>
2021-01-05 20:39:07 +0100

    Ajout du dossier simulateur proto

---
+++ Simulateur proto/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float thrust = 22000f;
+    private float turnSpeed = 4f;
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody rb;
+    [SerializeField] private GameObject PointG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = PointG.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // Move the vehicle forward
+
+        rb.AddRelativeForce(Vector3.forward * thrust * forwardInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput);
+    }
+}
