af0ad3422577e6966b99f96674e7e1048e6f3692
Rafouf69 <raphael.vignon@insa-lyon.fr>
2021-01-10 11:18:00 +0100

    changement dans carController

--- Simulateur proto/Assets/Scipts/PlayerController.cs
+++ Simulateur proto/Assets/Scipts/PlayerController.cs
@@ -1,33 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float thrust = 22000f;
-    private float turnSpeed = 4f;
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    
     private float horizontalInput;
-    private float forwardInput;
-    private Rigidbody rb;
-    [SerializeField] private GameObject PointG;
+    private float verticalInput;
+    private bool isBreaking;
+    private float steerAngle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = PointG.transform.position;
-    }
+    public float motorForce;
+    public float brakeForce;
+    public float maxSteeringAngle;
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform rearRightWheelTransform;
+
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        
-        // Move the vehicle forward
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        Debug.Log(horizontalInput + " / " + verticalInput);
+    }
 
-        rb.AddRelativeForce(Vector3.forward * thrust * forwardInput);
-        transform.Rotate(Vector3.up * turnSpeed * horizontalInput);
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        brakeForce = isBreaking ? 3000f : 0f;
+
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        steerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+        frontRightWheelCollider.steerAngle = steerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
